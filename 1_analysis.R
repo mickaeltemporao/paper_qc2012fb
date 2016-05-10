71a5285210f7eb574fcbc1863a6f045d346264a7
mickaeltemporao <mickael.temporao.1@ulaval.ca>
2016-05-10 16:14:30 -0400

    plots init

--- 1_analysis.R
+++ 1_analysis.R
@@ -5,13 +5,23 @@
 # Description:  TODO: (write me)
 # Version:      0.0.0.000
 # Created:      2016-05-10 13:01:26
-# Modified:     2016-05-10 13:06:04
+# Modified:     2016-05-10 16:13:56
 # Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
 # ------------------------------------------------------------------------------
 # Copyright (C) 2016 Mickael Temporão
 # Licensed under the GPL-2 < https://www.gnu.org/licenses/gpl-2.0.txt >
 # ------------------------------------------------------------------------------
 library(dplyr)
-db <- read.csv('data/2012_qc_lexicoder.csv') %>%
-  group_by(username)
+library(ggplot2)
+
+db <- read.csv('data/2012_qc_fb_posts.csv', stringsAsFactors=F) %>%
+  group_by(from_id) %>% select(-X)
+
+#1 Number of Posts (Official vs Non-Official) by Party
+plot1 <- db %>% dplyr::count(page_id, official)
+ggplot(plot1, aes(x=page_id, y=n, fill=official))+
+  geom_bar(stat='identity') +
+  scale_x_discrete(labels=c("coalitionavenir"="CAQ","lepartiquebecois"="PQ","LiberalQuebec"="PLQ",
+    "OptionNationale.QC"="ON","partivert"="PVQ","Quebecsolidaire"="QS")) +
+  scale_y_continuous("Nombre de posts")
 
--- src/0_prep_data.R
+++ src/0_prep_data.R
@@ -5,7 +5,7 @@
 # Description:  Preprocess Data for QC 2012 FB Analysis
 # Version:      0.0.0.000
 # Created:      2016-04-20 15:29:26
-# Modified:     2016-05-10 13:02:35
+# Modified:     2016-05-10 15:57:54
 # Author:       Mickael Temporão < mickael.temporao.1 at ulaval.ca >
 # ------------------------------------------------------------------------------
 # Copyright (C) 2016 Mickael Temporão
@@ -13,28 +13,48 @@
 # ------------------------------------------------------------------------------
 library(dplyr)
 
-# Functions --------------------------------------------------------------------
-unaccent <- function(text) {
-#YD Unaccent function
+path <- "../../../../../grcp/web/aspira/Elections_qc_2012_fb/Elections_Quebec_2012_FBPosts.csv"
+posts <- read.csv(path, encoding='UTF-8', stringsAsFactors=F, na.strings="None", sep=',')
+
+# Functions -------------------------------------------------------------------
+rm_diacritics <- function(text) {
   text <- gsub("['`^~\"]", "  ", text)
   text <- iconv(text, to="ASCII//TRANSLIT//IGNORE")
   text <- gsub("['`^~\"]", "", text)
   return(text)
 }
 
-# Loading Yannick Preprocessed Data frame --------------------------------------
-db <- data.table::fread("data/2012_fb_yd.csv", na.strings = "None") %>%
-  dplyr::filter(!is.na(message)) %>% select(-V1, -fid) %>%
-  as.data.frame()
-names(db) <- c('primary_post', 'username', 'from_username', 'message','likes','comments','picture', 'link', 'from_name')
-db$message <- unaccent(db$message)
+mv_lowercase <- function(x){
+  y <- NA
+  try_error <- tryCatch(tolower(x), error=function(e) e)
+  if(!inherits(try_error, "error"))
+    y <- tolower(x)
+  return(y)
+}
+
+# Preprocess ------------------------------------------------------------------
+
+# Official Posts by Parties - Official=1; Non-Official=0
+posts$official <- 0
+posts$official[posts$user__username==posts$ffrom__username] <- 1
+
+# Keep only required variables and posts on 6 majors FB Pages
+posts <- posts %>%
+  dplyr::filter(user__username %in% c("coalitionavenir", "LiberalQuebec", "OptionNationale.QC",
+    "lepartiquebecois", "partivert", "Quebecsolidaire")) %>%
+  dplyr::select(user__name, user__username, ffrom__name, ffrom__username, ffrom__gender, message,
+    official, picture, link, ftype, likes_count, comments_count, shares_count) %>%
+  dplyr::filter(message!="None") %>% rename(page=user__name, page_id=user__username,
+    from=ffrom__name, from_id=ffrom__username, gender=ffrom__gender, type=ftype)
+posts$message <- mv_lowercase(posts$message)
+posts$message <- rm_diacritics(posts$message)
 
 # Trying to find date variable in the other 200k posts...
 # filenames <- list.files(path='~/dropbox/grcp/web/aspira/elections_qc_2012_fb/', full.names=TRUE)[1:3]
 # datalist <- lapply(filenames, read.csv, header=T, stringsAsFactors=F)
-# db_full <- rbindlist(datalist,fill=T) %>% select(fid, created_time)
-# names(db_full) <- c('id','created')
-# match(db$id, db_full$id)
+# posts_full <- rbindlist(datalist,fill=T) %>% select(fid, created_time)
+# names(posts_full) <- c('id','created')
+# match(posts$id, posts_full$id)
 
 # Loading dictionaries & ignored features --------------------------------------
 # Loads yd_features; mt_features; dict_issues_fr
@@ -44,7 +64,7 @@ source('src/dictionaries_features.R')
 dict_fr <- dictionary(file = "frlsd.cat", format = "wordstat")
 
 # Generating the FB posts 2012 Corpus
-qc_corpus <- corpus(db, textField = "message")
+qc_corpus <- corpus(posts, textField = "message")
 summary(qc_corpus, 5)
 
 ## Test for word cloud
@@ -62,16 +82,15 @@ topfeatures(wc_dfm, 20)
 qc_dfm <- dfm(
   qc_corpus,
   dictionary = c(dict_fr, dict_issues_fr), # other dictionaries can be added ex. dict_parties
-  # ignoredFeatures = c(stopwords("french"),
-  #                     stopwords("english"),
-  #                     yd_features,
-  #                     mt_features),
+  ignoredFeatures = c(stopwords("french"),
+    stopwords("english"),
+    yd_features,
+    mt_features),
   stem = F, language = "french"
-  )
+)
 
-topfeatures(qc_dfm, 27)
+topfeatures(qc_dfm, 5)
 df_sent <- as.data.frame(qc_dfm)
 
-db <- db %>% bind_cols(df_sent)
-
-write.csv(db, 'data/2012_qc_lexicoder.csv')
+posts <- posts %>% bind_cols(df_sent)
+write.csv(posts, 'data/2012_qc_fb_posts.csv')
